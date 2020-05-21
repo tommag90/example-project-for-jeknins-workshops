29e3603b12aa63a64eeff0de2f6f4e86608b4a2d
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2020-05-21 12:21:41 +0200

    Added example class and test

---
+++ src/test/java/ogo/marcin/workshopstest/jenkinsworkshop/ExampleTest.java
@@ -0,0 +1,20 @@
+package ogo.marcin.workshopstest.jenkinsworkshop;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Marcin Ogorzalek
+ */
+
+public class ExampleTest {
+
+  @Test
+  public void shouldPassWhenRunAndNotModified() {
+    // Given
+    // When
+    // Then
+    assertTrue(true, "To pass test condition should be true, but was false");
+  }
+}
